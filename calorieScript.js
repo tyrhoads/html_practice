447db22a74681132a8108526bae3c125dba037f9
Tyler Rhoads <tyrhoads@gmail.com>
2024-08-20 22:37:20 -0400

    update

--- calorieScript.js
+++ calorieScript.js
@@ -7,7 +7,19 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
-  console.log("original string: ", str);
   const regex = /[+-\s]/g;
   return str.replace(regex, '');
+}
+
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+
+function addEntry() {
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />`;
 }
