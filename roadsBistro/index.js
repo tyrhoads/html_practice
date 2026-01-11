2eaebe78a9ff3e2f37e2dd6fc97efa82e092b736
Tyler Rhoads <tyrhoads@gmail.com>
2026-01-11 16:40:58 -0500

    click for the wheel spin is done

--- roadsBistro/index.js
+++ roadsBistro/index.js
@@ -1,1 +1,5 @@
 console.log('Happy developing ✨')
+const containter = document.getElementById("logoContainer");
+containter.addEventListener("click", () => {
+    containter.classList.toggle('spinning');
+})
